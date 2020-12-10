4bdb37a970e82e68e5e420d0c99fee83fb320c27
Erwin Santosa <erwincoc232@gmail.com>
2020-12-10 15:59:09 +0700

    Add project files.

---
+++ CRUDweb/Default.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CRUDweb._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <br />
+    <br />
+    <div>
+    <asp:Label ID="Label3" runat="server" Text="User Registration Form" BackColor="Maroon" Font-Names="Segoe UI" Font-Size="XX-Large" Font-Underline="False" ForeColor="White"></asp:Label>
+    </div>
+    <br />
+    User ID
+    <asp:TextBox ID="txtuid" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Name"></asp:Label>
+    &nbsp;&nbsp;&nbsp;
+<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Email"></asp:Label>
+&nbsp;&nbsp;&nbsp;
+<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+<br />
+<br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Insert" />
+    <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Search" />
+    <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Update" />
+    <asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Delete" />
+    <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Label ID="lblmsg" runat="server" Font-Bold="True" ForeColor="#003300" Text="Label"></asp:Label>
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+    <br />
+    <br />
+</asp:Content>
---
+++ CRUDweb/Default.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CRUDweb.ServiceReference1;
+
+namespace CRUDweb
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            InsertUser u = new InsertUser();
+            u.Name = TextBox1.Text;
+            u.Email = TextBox2.Text;
+            string r = client.Insert(u);
+            lblmsg.Text = r.ToString();
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.gettestdata gettestdata = new ServiceReference1.gettestdata();
+            gettestdata = client.GetInfo();
+            DataTable dataTable = new DataTable();
+            dataTable = gettestdata.usertab;
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            UpdateUser updateUser = new UpdateUser();
+            updateUser.UID = int.Parse(txtuid.Text);
+            updateUser.Name = TextBox1.Text;
+            updateUser.Email = TextBox2.Text;
+            string result = client.Update(updateUser);
+            lblmsg.Text = result.ToString();
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            DeleteUser deleteUser = new DeleteUser();
+            deleteUser.UID = int.Parse(txtuid.Text);
+            string res = client.Delete(deleteUser);
+            lblmsg.Text = res.ToString();
+        }
+    }
+}
